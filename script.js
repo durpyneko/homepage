785691a082a7cd0b4303b8430fab8be2e08f8e15
durpyneko <89787577+iDurpyDude12@users.noreply.github.com>
2023-05-25 23:58:58 +0200

    added bookmark dropdown and did some css changes

--- script.js
+++ script.js
@@ -6,6 +6,37 @@ settingButton = document.getElementById("openDialogButton");
 const dialog = document.querySelector("dialog");
 document.body.style.backgroundImage = `url(${background.value})`;
 const searchbar = document.getElementById("searchbar");
+const optionMenu = document.querySelector(".select-menu"),
+  selectBtn = optionMenu.querySelector(".select-btn"),
+  options = optionMenu.querySelectorAll(".option"),
+  sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+selectBtn.addEventListener("click", () =>
+  optionMenu.classList.toggle("active")
+);
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    let selectedOption = option.querySelector(".option-text").innerText;
+    sBtn_text.innerText = selectedOption;
+
+    optionMenu.classList.remove("active");
+  });
+});
+
+(function (document) {
+  var div = document.getElementById("addbookmark-dropdown");
+  var arrow = document.getElementById("arrow");
+  var open = false;
+  div.addEventListener("click", function () {
+    if (arrow.className == "arrow arrow-down") {
+      arrow.className = "arrow arrow-up";
+    } else if (arrow.className == "arrow arrow-up") {
+      arrow.className = "arrow arrow-down";
+    }
+    open = true;
+  });
+})(document);
 
 searchbar.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
